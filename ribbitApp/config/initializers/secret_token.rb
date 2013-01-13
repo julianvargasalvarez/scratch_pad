013cc95862ff597bd3ef8521f561b712e10587d6
Julian Vargas <julianvargasalvarez@gmail.com>
2013-01-12 22:09:33 -0500

    nada

--- ribbitApp/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RibbitApp::Application.config.secret_token = '573e12d61abaa385065da527641b3578fe73e1b5c83ce1e6791ea7544e575c71c5dbc2ba4aeffe0d90fa722fe879d6fa630c079930d13251073bc3e71a513840'
