2253855097617707d928b1210d40401f6dc47154
Julian Vargas <julianvargasalvarez@gmail.com>
2013-02-26 15:20:47 -0500

    initial tests for calculator

---
+++ tdd_python/python_test.py
@@ -0,0 +1,30 @@
+import unittest
+
+class Calculator(object):
+    def add(self, x, y):
+        number_types = (int, long, float, complex)
+        if isinstance(x, number_types) and isinstance(y, number_types):
+            return x + y
+        else:
+            raise ValueError
+
+class TddInPythonExample(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_calculator_add_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.add(2, 2)
+        self.assertEqual(4, result)
+    
+    def test_calculator_returns_error_message_if_both_args_not_numbers(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 'three')
+
+    def test_calculator_returns_error_message_if_x_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, 'two', 3)
+    
+    def test_calculator_returns_error_message_if_y_arg_not_number(self):
+        self.assertRaises(ValueError, self.calc.add, 2, 'three')
+
+if __name__ == "__main__":
+    unittest.main()
