dcd093dc64ec2a90a60a35c183f2f8de30894064
Julian Vargas <julianvargasalvarez@gmail.com>
2013-04-09 01:22:14 -0500

    some lanbdas in visual basic

---
+++ functional_programming_with_dot_net/Functions/Functions/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim a() = {Function(x) x + 1, Function(x) x * x, Function(x) x / 2}
+        For Each f In a
+            Console.WriteLine(f(3))
+        Next
+        Console.ReadKey()
+    End Sub
+End Module
