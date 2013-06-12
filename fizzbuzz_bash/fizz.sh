06d4ab7b73a4929296eff44cf712cc4f0f39989a
Julian Vargas <julianvargasalvarez@gmail.com>
2013-06-12 08:59:10 -0500

    fizzbuzz in bash 3

--- fizzbuzz_bash/fizz.sh
+++ fizzbuzz_bash/fizz.sh
@@ -3,17 +3,22 @@ function isDivisibleBy {
   return $(($1%$2))
 }
 
-for number in {1..100}; do
+function fizzOrBuzz {
     output=""
-    if isDivisibleBy $number 3; then
+    if isDivisibleBy $1 3; then
         output="Fizz"
     fi
-    if isDivisibleBy $number 5; then
+    if isDivisibleBy $1 5; then
         output="${output}Buzz"
     fi
     if [ -z $output ]; then
-        echo $number
+        echo $1
     else
         echo $output
     fi
+}
+
+for number in {1..100}; do
+    var=$(fizzOrBuzz $number)
+    echo $var
 done
