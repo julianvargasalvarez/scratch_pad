e2b5823a58ee843bf00710fa0480bb09edc48a1d
Julian Vargas <julianvargasalvarez@gmail.com>
2014-06-20 11:31:05 -0500

    adds lorenz

--- lorenz/lorenz.rb
+++ lorenz/lorenz.rb
@@ -4,14 +4,14 @@
 ##dz / dt = xy - c z
 ##P = 10, R = 28, c = 8 / 3.
 ##P = 28, R = 46.92, c = 4.
-# 
-MAX_ITERATION = 10000
+#
+MAX_ITERATION = 1000000
 h = 0.01;
 P = 10.0; #numero di Prandtl
 R = 28.0; #numero di Rayleigh
 c = 8.0 / 3.0;
 x, y, z = 0.1, 0, 0
- 
+
 0.upto(MAX_ITERATION) do |i|
   next_x = x + h * P * (y - x)
   next_y = y + h * (x * (R - z) - y)
